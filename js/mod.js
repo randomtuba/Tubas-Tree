dea92e23919705e75374731bba7952f0fe8fb43a
Glitch (tubas-tree) <none>
2021-07-31 00:41:20 +0000

    🌸🌪 Updated with Glitch

--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "randomtuba's Discord Server",
 	discordLink: "https://discord.gg/VxHzjtgdA4",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (0), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
